dd860bb666d17c3b354bfb4331b64689d050b877
th816 <th816@naver.com>
2022-11-29 15:21:14 +0900

    토큰

---
+++ 10_API/src/com/kh/chap02_string/controller/A_StringPoolTest.java
@@ -0,0 +1,73 @@
+package com.kh.chap02_string.controller;
+
+public class A_StringPoolTest {
+
+	   
+	   // 생성자를 통해 문자열 담기
+	   public void method1() {
+	      
+	      String str1 = new String("Hello");
+	      String str2 = new String("Hello");
+	      
+	      System.out.println(str1 == str2); // false => 주소값 비교
+	      System.out.println(str1);
+	      System.out.println(str2.toString());
+	      // String 클래스의 toString()메소드의 경우 실제 담겨있는 문자열을 반환하도록 오버라이딩 되어있음.
+	      
+	      System.out.println(str1.equals(str2)); // true => 문자열 비교
+	      // String 클래스의 equals()메소드의 경우 주소값 비교가 아닌 문자열 비교를 하도록 오버라이딩 되어있음.
+	      
+	      System.out.println(str1.hashCode());
+	      System.out.println(str2.hashCode());
+	      // String클래스의 hashCode()메소드의 경우 주소값 기반이 아닌 실제 담긴 문자열 기반으로 해시코드 값을
+	      // 반환하도록 오버라이딩 되어있음.
+	      
+	      // 정말 주소값을 알고싶을 때 사용하는 메서드 System.identityhashCode(참조변수(레퍼런스변수));
+	      System.out.println(System.identityHashCode(str1));
+	      System.out.println(System.identityHashCode(str2));
+	      
+	   }
+	   
+	   // 2. 문자열을 리터럴(값)로 생성
+	   public void method2() {
+	      
+	      String str = new String("hello");
+	      
+	      // 리터럴값이 상수풀에 올라감.
+	      String str1 = "hello";
+	      String str2 = "hello";
+	      
+	      // StringPool : 동일한 문자열 존재 불가
+	      System.out.println(str1 == str2); // true(주소값이 일치한다)
+	      
+	      System.out.println(str1);
+	      System.out.println(str2);
+	      
+	      System.out.println(str1.hashCode());
+	      System.out.println(str2.hashCode());
+	      
+	      System.out.println(System.identityHashCode(str1));
+	      System.out.println(System.identityHashCode(str2));
+	   }
+	   
+	   // 불변클래스
+	   public void method3() {
+	      String str = "hello";
+	      System.out.println(System.identityHashCode(str));
+	      
+	      str = "goodbye";
+	      System.out.println(System.identityHashCode(str));
+	      
+	      str += "abc"; //str = atr + "abc";
+	      System.out.println(System.identityHashCode(str));
+	      
+	      /*
+	       * 기존의 상수풀의 연결이 끊긴 문자열들은 가비지 콜렉터가 알아서 정리해줌
+	       * 불변이라고 해서 수정이 아예 안되는게 아니라
+	       * 기존의 그 자리(원래있던 주소값)에서 수정되는 개념이 아니라는 것.
+	       * 매번 새로운 주소값을 참조하게 됨.
+	       */
+	      
+	   }
+
+	}
---
+++ 10_API/src/com/kh/practice/token/controller/TokenController.java
@@ -0,0 +1,35 @@
+package com.kh.practice.token.controller;
+
+import java.util.StringTokenizer;
+
+public class TokenController {
+	
+	public TokenController() {
+		
+		
+	}
+	
+	public String afterToken(String str) {
+		
+		
+		StringTokenizer stn = new StringTokenizer(str,",");
+		stn.countTokens();
+		return "";
+		
+	}
+	
+	public String firstCap(String input) {
+		
+		return "";
+	}
+	
+	public int findChar(String intput, char one) {
+		
+		return 1;
+	}
+	
+	
+	
+	
+	
+}
